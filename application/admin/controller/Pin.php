5c7a512c638dacc7606329c553520f0128fb3d35
lvzhihui <lvzh@cloud-young.com>
2018-10-24 13:08:10 +0800

    second

--- application/admin/controller/Pin.php
+++ application/admin/controller/Pin.php
@@ -9,9 +9,11 @@
 namespace app\admin\controller;
 
 
+use app\common\controller\AdminBase;
 use app\common\model\Order;
 use app\common\model\PorderDetail;
 use app\common\model\PorderList;
+use think\Config;
 use think\Controller;
 
 /**
@@ -19,7 +21,7 @@ use think\Controller;
  * Class Pin
  * @package app\admin\controller
  */
-class Pin extends Controller
+class Pin extends AdminBase
 {
 
     /**
@@ -46,11 +48,43 @@ class Pin extends Controller
      * 数据插入
      * */
     public function insertorderdata(){
-        $data = request()->post('data','');
+        $client_id = Config::get('client_id');
+        $secret = Config::get('client_secret');
+
+        $param['client_id'] = $client_id;
+        $param['type'] =  'pdd.ddk.order.list.increment.get';
+        $param['data_type'] = 'JSON';
+        $param['timestamp'] = time();
+        $param['start_update_time'] = time()-(24*3600);
+        $param['end_update_time'] =  time();
+
+        ksort($param);    //  排序
+        $str = '';      //  拼接的字符串
+        foreach ($param as $k => $v) $str .= $k . $v;
+        $sign = strtoupper(md5($secret. $str . $secret));    //  生成签名    MD5加密转大写
+        $param['sign'] = $sign;
+        $url = 'http://gw-api.pinduoduo.com/api/router';
+        $data = $this->curl_post($url,$param);
+
         if(!isset($data) && empty($data)) return false;
 
         $res = PorderList::insertdata($data);
         return $res;
+    }
+
+    public function curl_post($url,$curlPost){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_REFERER, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
     }
 
 
