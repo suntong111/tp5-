7da7648e7baefea2f5b32949006534639a5ee96b
suntong111 <1372880535@qq.com>
2018-10-24 22:03:38 +0800

    更新定时器

---
+++ application/common/model/News.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sunt
+ * Date: 2018/10/24
+ * Time: 20:54
+ */
+
+namespace app\common\model;
+
+
+use think\Model;
+
+class News extends Model
+{
+
+}
